7d54633a5fbd39f4a84b859e7f65d740b69618ba
2DevGs <dev.gustavoserrano@gmail.com>
2025-06-30 14:53:10 -0300

    Understanding Concept
    In this commit, a simple navigation was implemented to demonstrate a basic example of dependency management.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_dependencias/pages/basico/basico_home_page.dart';
+import 'package:get_dependencias/pages/home_page.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -7,6 +9,17 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp();
+    return GetMaterialApp(
+      getPages: [
+        GetPage(
+          name: '/', 
+          page: () => const HomePage(),
+        ),
+        GetPage(
+          name: '/basico', 
+          page: () => BasicoHomePage(),
+        ),
+      ],
+    );
   }
 }
